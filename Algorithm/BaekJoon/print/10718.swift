f9517c163859ecbd14e82597d1c9be1dd601d79c
Nick(Seungyun Kim) <sy5072@naver.com>
2022-06-29 00:03:22 +0900

    [백준 10718] We love kriii

--- Algorithm/BaekJoon/print/10718.swift
+++ Algorithm/BaekJoon/print/10718.swift
@@ -1,8 +1,8 @@
 //
-//  main.swift
+//  10718.swift
 //  Algorithm
 //
-//  Created by Seungyun Kim on 2022/06/28.
+//  Created by Seungyun Kim on 2022/06/29.
 //
 
 import Foundation
---
+++ Algorithm/BaekJoon/print/2557.swift
@@ -0,0 +1,10 @@
+//
+//  2557.swift
+//  Algorithm
+//
+//  Created by Seungyun Kim on 2022/06/29.
+//
+
+import Foundation
+
+print("Hello World")
