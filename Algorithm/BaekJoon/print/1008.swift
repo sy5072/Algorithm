28be5839054afec4c65bcaeab0f733efadf49724
Nick(Seungyun Kim) <sy5072@naver.com>
2022-09-29 21:47:15 +0900

    [백준 1008] A/B

---
+++ Algorithm/BaekJoon/print/1008.swift
@@ -0,0 +1,15 @@
+//
+//  1008.swift
+//  Algorithm
+//
+//  Created by Seungyun Kim on 2022/09/29.
+//
+
+import Foundation
+
+let input = readLine()!
+let strings = input.components(separatedBy: " ")
+let a = Double(strings[0])!
+let b = Double(strings[1])!
+
+print(a/b)
