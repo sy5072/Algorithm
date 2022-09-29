75def47df9ba234997918bb8dc208c8a6517a4c6
Nick(Seungyun Kim) <sy5072@naver.com>
2022-09-29 21:45:01 +0900

    [백준 1000] A+B

--- Algorithm/BaekJoon/main.swift
+++ Algorithm/BaekJoon/main.swift
@@ -7,4 +7,9 @@
 
 import Foundation
 
-print("강한친구 대한육군\n강한친구 대한육군")
+let input = readLine()!
+let strings = input.components(separatedBy: " ")
+let a = Int(strings[0])!
+let b = Int(strings[1])!
+
+print(a+b)
---
+++ Algorithm/BaekJoon/print/1000.swift
@@ -0,0 +1,15 @@
+//
+//  1000.swift
+//  Algorithm
+//
+//  Created by Seungyun Kim on 2022/09/29.
+//
+
+import Foundation
+
+let input = readLine()!
+let Strings = input.components(separatedBy: " ")
+let a = Int(String[0])!
+let b = Int(String[1])!
+
+print(a+b)
