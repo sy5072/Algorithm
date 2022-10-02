b2e6fb453f17cc98e7bb0f06f191c49c27a4921a
Nick(Seungyun Kim) <sy5072@naver.com>
2022-10-02 13:12:26 +0900

    파일 생성 누락

---
+++ Algorithm/BaekJoon/print/2884.swift
@@ -0,0 +1,20 @@
+//
+//  2994.swift
+//  Algorithm
+//
+//  Created by Seungyun Kim on 2022/10/02.
+//
+
+import Foundation
+
+let input = readLine()!.components(separatedBy: " ")
+let a = Int(input[0])!
+let b = Int(input[1])!
+
+let totalMinute = a * 60 + b
+let setTime = 24 * 60 + (totalMinute - 45)
+
+let c = setTime / 60 % 24
+let d = setTime % 60
+
+print(c, d, terminator: " ")
