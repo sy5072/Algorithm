3c73aa313065af894fbfceaee931a5b68c003507
Nick(Seungyun Kim) <sy5072@naver.com>
2022-10-31 21:17:16 +0900

    [백준 5597] 과제 안 내신 분...?

--- Algorithm/BaekJoon/1차원 배열/5597.swift
+++ Algorithm/BaekJoon/1차원 배열/5597.swift
@@ -1,8 +1,8 @@
 //
-//  main.swift
+//  5597.swift
 //  Algorithm
 //
-//  Created by Seungyun Kim on 2022/06/28.
+//  Created by Seungyun Kim on 2022/10/31.
 //
 
 var attendanceNumber = [Int](1...30)
@@ -13,3 +13,4 @@ for _ in 1...3 {
 
 print(attendanceNumber.min()!)
 print(attendanceNumber.max()!)
+
