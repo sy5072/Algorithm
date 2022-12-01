fde0c4bc2640087049e7d5ca23dd6a295f62722c
Nick(Seungyun Kim) <sy5072@naver.com>
2022-12-01 17:12:12 +0900

    [백준 2675] 문자열 반복

--- Algorithm/BaekJoon/문자열/2675.swift
+++ Algorithm/BaekJoon/문자열/2675.swift
@@ -1,8 +1,8 @@
 //
-//  main.swift
+//  2675.swift
 //  Algorithm
 //
-//  Created by Seungyun Kim on 2022/06/28.
+//  Created by Seungyun Kim on 2022/12/01.
 //
 
 let inputCount = Int(readLine()!)!
