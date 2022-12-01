6a37a3e23aeed507d65a5b4abac50efdd2274300
Nick(Seungyun Kim) <sy5072@naver.com>
2022-12-01 18:07:02 +0900

    [백준 5622] 다이얼

--- Algorithm/BaekJoon/문자열/5622.swift
+++ Algorithm/BaekJoon/문자열/5622.swift
@@ -1,8 +1,8 @@
 //
-//  main.swift
+//  5622.swift
 //  Algorithm
 //
-//  Created by Seungyun Kim on 2022/06/28.
+//  Created by Seungyun Kim on 2022/12/01.
 //
 
 func toTakeNumber(char: Character) -> Int {
