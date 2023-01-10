a2c4bc7790d1044737a9f694d4a2a74f400bdc64
Nick(Seungyun Kim) <sy5072@naver.com>
2023-01-10 11:42:37 +0900

    [백준 10250] CMA 호텔

--- Algorithm/BaekJoon/기본수학1/10250.swift
+++ Algorithm/BaekJoon/기본수학1/10250.swift
@@ -1,8 +1,8 @@
 //
-//  main.swift
+//  10250.swift
 //  Algorithm
 //
-//  Created by Seungyun Kim on 2022/06/28.
+//  Created by Seungyun Kim on 2023/01/10.
 //
 
 var testCount = Int(readLine()!)!
